f506e55715ecfdb62d0c637b416b186837ca125a
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-05-23 20:58:51 +0545

    .

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -8,6 +8,7 @@ export const AllRoutes = () => {
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/products" element={<ProductsList />} />
+                <Route path="/products/:id" element={<ProductsList />} />
             </Routes>
         </>
     )
