0387827afaea9b6d41c9497f005bff25dc81a7c6
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-05-25 13:51:28 +0545

    .

--- src/pages/Products/ProductsList.js
+++ src/pages/Products/ProductsList.js
@@ -1,18 +1,21 @@
 import { useEffect, useState } from "react"
 import { ProductCard } from "../../components";
 import { FilterBar } from './components/FilterBar'
+import { useLocation } from "react-router-dom";
 
 export const ProductsList = () => {
   const [show, setShow] = useState(false)
   const [products, setProducts] = useState([]);
+  const search = useLocation().search;
+  const searchTerm = new URLSearchParams(search).get("q");
   useEffect(() => {
     async function fetchProducts() {
-      const response = await fetch("http://localhost:8000/products");
+      const response = await fetch(`http://localhost:8000/products?name_like=${searchTerm ? searchTerm : ""}`);
       const data = await response.json()
       setProducts(data)
     }
     fetchProducts();
-  },[])
+  }, [])
   return (
     <main>
       <section className="my-5">
@@ -28,7 +31,7 @@ export const ProductsList = () => {
           {
             products.map((product) =>
             (
-              <ProductCard product={product} key={product.id}/>
+              <ProductCard product={product} key={product.id} />
             ))
           }
         </div>
