86303eeb94a33f2d2f5fef14f6d31ea662de73ad
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-05-25 10:15:18 +0545

    .

--- src/components/Other/ScrollToTop.js
+++ src/components/Other/ScrollToTop.js
@@ -1,5 +1,12 @@
-export const ScrollToTop = () => {
-  return (
-    <div>ScrollToTop</div>
-  )
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export const ScrollToTop=()=> {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { ScrollToTop } from './components';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </React.StrictMode>
