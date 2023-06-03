11dc292d33c309449872b142b22e083868551619
Nit-Sub <52620899+Nit-Sub@users.noreply.github.com>
2023-06-03 19:03:15 +0545

    Update FilterBar.js

--- src/pages/Products/components/FilterBar.js
+++ src/pages/Products/components/FilterBar.js
@@ -6,7 +6,7 @@ export const FilterBar = ({ setShow }) => {
         <button onClick={() => setShow(false)} type="button" data-drawer-dismiss="drawer-disable-body-scrolling" aria-controls="drawer-disable-body-scrolling" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white">
           <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
           <span className="sr-only">Close</span>
-        </button>
+      </button>
         <div className="border-b pb-3"></div>
         <div className="py-4 overflow-y-auto">
           <ul className="text-slate-700 dark:text-slate-100">
